91fc795e8d5de95ddd07849e94a1b3e92f6fd488
YordyHc <yordyhuamanc@gmail.com>
2025-06-17 17:45:41 -0500

    feat: agregar logica para mostrar error y diseño de la pantalla de carga

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,9 @@ import "./assets/galeria.css";
 function App() {
   const [channel, setChannel] = useState(null);
   const [videos, setVideos] = useState([]);
-
   const [channelId, setChannelId] = useState(null);
+  const [error, setError] = useState(null);
+
   const handle = appConfig.handle;
   const BACKEND_BASE_URL =
     "https://api-widget-youtube.onrender.com/api/youtube/youtube";
@@ -19,29 +20,61 @@ function App() {
     if (!handle) return;
 
     fetch(`${BACKEND_BASE_URL}/channel-info/?forHandle=${handle}`)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok)
+          throw new Error("No se pudo obtener la información del canal");
+        return res.json();
+      })
       .then((data) => {
         setChannel(data);
         if (data.idcanal) {
           setChannelId(data.idcanal);
         }
       })
-      .catch((err) => console.error("Error al cargar canal:", err));
+      .catch((err) => {
+        console.error("Error al cargar canal:", err);
+        setError("❌ Error al cargar información del canal.");
+      });
   }, []);
 
   useEffect(() => {
     if (!channelId) return;
 
     fetch(`${BACKEND_BASE_URL}/videos-info/?channelId=${channelId}`)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) throw new Error("No se pudieron obtener los videos");
+        return res.json();
+      })
       .then((data) => {
         setVideos(data.videos);
       })
-      .catch((err) => console.error("Error al cargar videos:", err));
+      .catch((err) => {
+        console.error("Error al cargar videos:", err);
+        setError("❌ Error al cargar los videos del canal.");
+      });
   }, [channelId]);
 
+  if (error) {
+    return (
+      <div className="container mt-5">
+        <div className="alert alert-danger text-center" role="alert">
+          {error}
+        </div>
+      </div>
+    );
+  }
+
   if (!channel || videos.length === 0) {
-    return <p>Cargando...</p>;
+    return (
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <div className="text-center">
+          <div className="spinner-border text-primary mb-3" role="status">
+            <span className="visually-hidden">Cargando...</span>
+          </div>
+          <div className="fw-bold">Cargando contenido del canal...</div>
+        </div>
+      </div>
+    );
   }
 
   return (
