5a9de7a17a85fae01ff613ce9d398793c9f918d5
YordyHc <yordyhuamanc@gmail.com>
2025-06-17 02:11:05 -0500

    refactor: eliminando carpetas obsoletas

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('API_KEY', 'ingresa_clave_api');
-define('CHANNEL_ID', 'ingresa_id_canal');
-/*
-id de canal de prueba 1: UCCfPkh8osJPC2pPq283kKXg
-id de canal de prueba 2: UCsMzBY3X_RS6GhGBiHXW19A
-*/
